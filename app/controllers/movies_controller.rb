1eca1e53b312cb404ba786dc2028194ddda66b41
Brianna Bell <bbell9563@gmail.com>
2020-03-07 10:27:45 -0700

    working on commits model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,22 +8,32 @@ class MoviesController < ApplicationController
   end
 
   def new
-    @movie = 
+    @movie = Movie.new
   end
 
   def create
-
+    @movie = Movie.new(movies_params)
+    if @movie.save
+      redirect_to movies_path
+    else  
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-
+    if @movie.update(movies_params)
+      redirect_to movie_path(@movie)
+    else 
+      render :edit
+    end
   end
 
   def destroy
     @movie.destroy 
+    redirect_to movies_path
   end
 
   private
