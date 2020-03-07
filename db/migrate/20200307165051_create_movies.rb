b0a2b29f90931e3e2fded69719fd4c32e450a6e8
Brianna Bell <bbell9563@gmail.com>
2020-03-07 09:52:24 -0700

    controllers and models made

---
+++ db/migrate/20200307165051_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :duration
+      t.string :genre
+      t.string :description
+      t.string :trailor
+
+      t.timestamps
+    end
+  end
+end
